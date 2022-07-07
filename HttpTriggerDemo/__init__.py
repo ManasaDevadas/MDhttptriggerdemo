cb54e53fe9ea2b286fbe0044c87ab162d5811944
Manasa Devadas <madevada@microsoft.com>
2022-07-07 23:52:43 +0530

    demo

---
+++ HttpTriggerDemo/__init__.py
@@ -0,0 +1,34 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    place = req.params.get('place')
+
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if not place:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            place = req_body.get('place')
+
+    if name and place :
+        return func.HttpResponse(f"Hello, {name} from {place}. The function is triggered successfully")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name and place in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
